7fff4671f4630c5c3f571fb9e531327555c6373c
DanielJang <daniel.jang.0621@gmail.com>
2021-01-18 03:32:18 +0900

    fixed netflix icon bug

--- src/navbar/Nav.js
+++ src/navbar/Nav.js
@@ -40,7 +40,7 @@ function Nav() {
             <div>
                 <img
                     className={"logo"}
-                    src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
                     alt="Netflix Logo"
                 />
             </div>
