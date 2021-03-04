411dd1dd81ad44e2ce7e5f6b63618846b2723f42
DanielJang <daniel.jang.0621@gmail.com>
2021-03-04 17:29:28 +0900

    implemented code splitting for routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from "react";
-import Home from "./pages/Home";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import Search from "./pages/Search";
-import Movie from "./pages/Movie";
-import TvShow from "./pages/TvShow";
 import Nav from "./navbar/Nav";
+import Search from "./pages/Search";
+const Home = lazy(() => import("./pages/Home"));
+// const Search = lazy(() => import("./pages/Search"));
+const Movie = lazy(() => import("./pages/Movie"));
+const TvShow = lazy(() => import("./pages/TvShow"));
 
 const App = () => {
     return (
         <div>
             <Nav />
             <Switch>
-                <Route path="/Netflix_Clone/home" exact component={Home} />
-                <Route path="/Netflix_Clone/search" exact component={Search} />
-                <Route path="/Netflix_Clone/movies" exact component={Movie} />
-                <Route path="/Netflix_Clone/tvshows" exact component={TvShow} />
-                <Redirect from="/" to="/Netflix_Clone/home" />
+                <Suspense
+                    fallback={
+                        <div style={{ height: "1500px", color: "black" }}></div>
+                    }
+                >
+                    <Route path="/Netflix_Clone/home" exact component={Home} />
+                    <Route
+                        path="/Netflix_Clone/search"
+                        exact
+                        component={Search}
+                    />
+                    <Route
+                        path="/Netflix_Clone/movies"
+                        exact
+                        component={Movie}
+                    />
+                    <Route
+                        path="/Netflix_Clone/tvshows"
+                        exact
+                        component={TvShow}
+                    />
+                    <Redirect from="/" to="/Netflix_Clone/home" />
+                </Suspense>
             </Switch>
         </div>
     );
